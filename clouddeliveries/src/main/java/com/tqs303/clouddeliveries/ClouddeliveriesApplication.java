73f2f911879fc0715aa78da5db15423c2394ac0c
Diego <diegotrovisco@ua.pt>
2019-03-29 11:32:48 +0000

    inicial

---
+++ clouddeliveries/src/main/java/com/tqs303/clouddeliveries/ClouddeliveriesApplication.java
@@ -0,0 +1,13 @@
+package com.tqs303.clouddeliveries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClouddeliveriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClouddeliveriesApplication.class, args);
+	}
+
+}
