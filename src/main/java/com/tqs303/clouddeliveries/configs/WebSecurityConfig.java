cdd7cb3609e0c77e9be8530cba6a9d63a4f3be7b
hjmmansoUA <38533847+hjmmansoUA@users.noreply.github.com>
2019-05-24 11:46:06 +0100

    Update WebSecurityConfig.java

--- src/main/java/com/tqs303/clouddeliveries/configs/WebSecurityConfig.java
+++ src/main/java/com/tqs303/clouddeliveries/configs/WebSecurityConfig.java
@@ -1,14 +1,16 @@
 package com.tqs303.clouddeliveries.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
