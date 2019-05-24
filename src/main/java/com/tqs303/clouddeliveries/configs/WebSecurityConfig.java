a3f5299f0887194ccc3e737a853345dacbf901e9
hjmmansoUA <38533847+hjmmansoUA@users.noreply.github.com>
2019-05-24 11:44:05 +0100

    Update WebSecurityConfig.java

--- src/main/java/com/tqs303/clouddeliveries/configs/WebSecurityConfig.java
+++ src/main/java/com/tqs303/clouddeliveries/configs/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-        .antMatchers("/pedido", "/verpedidos")
+        .antMatchers("/pedido" , "/verpedidos")
         .authenticated()
         .antMatchers("/", "index", , "information" , "register")
         .permitAll()
